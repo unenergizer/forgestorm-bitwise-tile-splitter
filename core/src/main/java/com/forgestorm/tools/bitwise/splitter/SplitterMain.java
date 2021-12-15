2aee2fc468aafe93a1eb6020e0250ed5b8f8cc43
unenergizer <unenergizer@gmail.com>
2021-12-14 18:51:28 -0600

    Added compact bitwise 48 mountain tiles

--- core/src/main/java/com/forgestorm/tools/bitwise/splitter/SplitterMain.java
+++ core/src/main/java/com/forgestorm/tools/bitwise/splitter/SplitterMain.java
@@ -18,14 +18,16 @@ import lombok.Getter;
 
 public class SplitterMain extends ApplicationAdapter {
 
-    private static final BitWiseSplitType SPLIT_TYPE = BitWiseSplitType.SIXTEEN;
+    private static final BitWiseSplitType SPLIT_TYPE = BitWiseSplitType.SIXTEEN_MTN_WALL;
 
     @Getter
     @AllArgsConstructor
     public enum BitWiseSplitType {
-        FOUR("BW4", "input/BW4/", 4, 4, 16, 16),
+        FOUR_ORIGINAL("BW4", "input/BW4/", 4, 4, 16, 16),
         FOUR_DOUBLE("BW4", "input/BW4H32/", 4, 4, 16, 32),
-        SIXTEEN("BW16", "input/BW16/", 10, 5, 16, 16);
+
+        SIXTEEN_ORIGINAL("BW16", "input/BW16/", 10, 5, 16, 16),
+        SIXTEEN_MTN_WALL("BW16", "input/BW16MTN/", 5, 3, 16, 16);
 
         private final String fileNamePrefix;
         private final String getDirectory;
@@ -39,80 +41,122 @@ public class SplitterMain extends ApplicationAdapter {
     private static final String ATLAS_OUTPUT_DIR = "atlas/";
     private static final String PNG = ".png";
 
-    private final HashMap<Integer, Integer> hashMapBW4;
-    private final HashMap<Integer, Integer> hashMapBW16;
+    private final HashMap<Integer, Integer> bitWise16Full;
+    private final HashMap<Integer, Integer> bitWise48Full;
+    private final HashMap<Integer, Integer> bitWise48MtnWall;
 
     public SplitterMain() {
-        hashMapBW4 = new HashMap<>();
+        // Original full 16
+        bitWise16Full = new HashMap<>();
         // Row 1
-        hashMapBW4.put(0, 5);
-        hashMapBW4.put(1, 7);
-        hashMapBW4.put(2, 3);
-        hashMapBW4.put(3, 1);
-        // Row 2
-        hashMapBW4.put(4, 13);
-        hashMapBW4.put(5, 15);
-        hashMapBW4.put(6, 11);
-        hashMapBW4.put(7, 9);
-        // Row 3
-        hashMapBW4.put(8, 12);
-        hashMapBW4.put(9, 14);
-        hashMapBW4.put(10, 10);
-        hashMapBW4.put(11, 8);
-        // Row 4
-        hashMapBW4.put(12, 4);
-        hashMapBW4.put(13, 6);
-        hashMapBW4.put(14, 2);
-        hashMapBW4.put(15, 0);
+        bitWise16Full.put(0, 5);
+        bitWise16Full.put(1, 7);
+        bitWise16Full.put(2, 3);
+        bitWise16Full.put(3, 1);
 
-        hashMapBW16 = new HashMap<>();
-        hashMapBW16.put(0, 22);
-        hashMapBW16.put(1, 31);
-        hashMapBW16.put(2, 11);
-        hashMapBW16.put(3, 2);
-        hashMapBW16.put(4, 18);
-        hashMapBW16.put(5, 26);
-        hashMapBW16.put(6, 10);
-        hashMapBW16.put(7, 251);
-        hashMapBW16.put(8, 250);
-        hashMapBW16.put(9, 254);
-        hashMapBW16.put(10, 214);
-        hashMapBW16.put(11, 255);
-        hashMapBW16.put(12, 107);
-        hashMapBW16.put(13, 66);
-        hashMapBW16.put(14, 82);
-        hashMapBW16.put(15, 90);
-        hashMapBW16.put(16, 74);
-        hashMapBW16.put(17, 123);
-        hashMapBW16.put(19, 222);
-        hashMapBW16.put(20, 208);
-        hashMapBW16.put(21, 248);
-        hashMapBW16.put(22, 104);
-        hashMapBW16.put(23, 64);
-        hashMapBW16.put(24, 80);
-        hashMapBW16.put(25, 88);
-        hashMapBW16.put(26, 72);
-        hashMapBW16.put(27, 127);
-        hashMapBW16.put(28, 95);
-        hashMapBW16.put(29, 223);
-        hashMapBW16.put(30, 16);
-        hashMapBW16.put(31, 24);
-        hashMapBW16.put(32, 8);
-        hashMapBW16.put(33, 0);
-        hashMapBW16.put(34, 27);
-        hashMapBW16.put(35, 106);
-        hashMapBW16.put(36, 210);
-        hashMapBW16.put(37, 30);
-        hashMapBW16.put(38, 122);
-        hashMapBW16.put(39, 218);
-        hashMapBW16.put(42, 126);
-        hashMapBW16.put(43, 219);
-        hashMapBW16.put(44, 86);
-        hashMapBW16.put(45, 216);
-        hashMapBW16.put(46, 120);
-        hashMapBW16.put(47, 75);
-        hashMapBW16.put(48, 91);
-        hashMapBW16.put(49, 94);
+        // Row 2
+        bitWise16Full.put(4, 13);
+        bitWise16Full.put(5, 15);
+        bitWise16Full.put(6, 11);
+        bitWise16Full.put(7, 9);
+
+        // Row 3
+        bitWise16Full.put(8, 12);
+        bitWise16Full.put(9, 14);
+        bitWise16Full.put(10, 10);
+        bitWise16Full.put(11, 8);
+
+        // Row 4
+        bitWise16Full.put(12, 4);
+        bitWise16Full.put(13, 6);
+        bitWise16Full.put(14, 2);
+        bitWise16Full.put(15, 0);
+
+        // Original full 48
+        bitWise48Full = new HashMap<>();
+        // Row 1
+        bitWise48Full.put(0, 22);
+        bitWise48Full.put(1, 31);
+        bitWise48Full.put(2, 11);
+        bitWise48Full.put(3, 2);
+        bitWise48Full.put(4, 18);
+        bitWise48Full.put(5, 26);
+        bitWise48Full.put(6, 10);
+        bitWise48Full.put(7, 251);
+        bitWise48Full.put(8, 250);
+        bitWise48Full.put(9, 254);
+
+        // Row 2
+        bitWise48Full.put(10, 214);
+        bitWise48Full.put(11, 255);
+        bitWise48Full.put(12, 107);
+        bitWise48Full.put(13, 66);
+        bitWise48Full.put(14, 82);
+        bitWise48Full.put(15, 90);
+        bitWise48Full.put(16, 74);
+        bitWise48Full.put(17, 123);
+        // Skip 18 it is blank
+        bitWise48Full.put(19, 222);
+
+        // Row 3
+        bitWise48Full.put(20, 208);
+        bitWise48Full.put(21, 248);
+        bitWise48Full.put(22, 104);
+        bitWise48Full.put(23, 64);
+        bitWise48Full.put(24, 80);
+        bitWise48Full.put(25, 88);
+        bitWise48Full.put(26, 72);
+        bitWise48Full.put(27, 127);
+        bitWise48Full.put(28, 95);
+        bitWise48Full.put(29, 223);
+
+        // Row 4
+        bitWise48Full.put(30, 16);
+        bitWise48Full.put(31, 24);
+        bitWise48Full.put(32, 8);
+        bitWise48Full.put(33, 0);
+        bitWise48Full.put(34, 27);
+        bitWise48Full.put(35, 106);
+        bitWise48Full.put(36, 210);
+        bitWise48Full.put(37, 30);
+        bitWise48Full.put(38, 122);
+        bitWise48Full.put(39, 218);
+
+        // Row 5
+        // Skip 40 it is blank
+        // Skip 41 it is blank
+        bitWise48Full.put(42, 126);
+        bitWise48Full.put(43, 219);
+        bitWise48Full.put(44, 86);
+        bitWise48Full.put(45, 216);
+        bitWise48Full.put(46, 120);
+        bitWise48Full.put(47, 75);
+        bitWise48Full.put(48, 91);
+        bitWise48Full.put(49, 94);
+
+
+        // Modified bitwise 48 for compact mountain wall
+        bitWise48MtnWall = new HashMap<>();
+        // Row 1
+        bitWise48MtnWall.put(0, 22);
+        bitWise48MtnWall.put(1, 31);
+        bitWise48MtnWall.put(2, 11);
+        bitWise48MtnWall.put(3, 251);
+        bitWise48MtnWall.put(4, 254);
+
+        // Row 2
+        bitWise48MtnWall.put(5, 214);
+        bitWise48MtnWall.put(6, 255);
+        bitWise48MtnWall.put(7, 107);
+        bitWise48MtnWall.put(8, 127);
+        bitWise48MtnWall.put(9, 223);
+
+        // Row 3
+        bitWise48MtnWall.put(10, 208);
+        bitWise48MtnWall.put(11, 248);
+        bitWise48MtnWall.put(12, 104);
+        bitWise48MtnWall.put(13, 126);
+        bitWise48MtnWall.put(14, 219);
     }
 
     @Override
@@ -138,8 +182,8 @@ public class SplitterMain extends ApplicationAdapter {
             int tilesSavedToDisk = 0;
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
-                    if (hashMapBW4.get(total) == null && SPLIT_TYPE == BitWiseSplitType.FOUR
-                            || hashMapBW16.get(total) == null && SPLIT_TYPE == BitWiseSplitType.SIXTEEN) {
+                    if (bitWise16Full.get(total) == null && SPLIT_TYPE == BitWiseSplitType.FOUR_ORIGINAL
+                            || bitWise48Full.get(total) == null && SPLIT_TYPE == BitWiseSplitType.SIXTEEN_ORIGINAL) {
                         total++;
                         continue;
                     }
@@ -164,12 +208,15 @@ public class SplitterMain extends ApplicationAdapter {
                     // Get the file name
                     String updatedName = null;
                     switch (SPLIT_TYPE) {
-                        case FOUR:
+                        case FOUR_ORIGINAL:
                         case FOUR_DOUBLE:
-                            updatedName = IMAGE_OUTPUT_DIR + SPLIT_TYPE.getFileNamePrefix() + "-" + fileName + "-" + hashMapBW4.get(total) + PNG;
+                            updatedName = IMAGE_OUTPUT_DIR + SPLIT_TYPE.getFileNamePrefix() + "-" + fileName + "-" + bitWise16Full.get(total) + PNG;
                             break;
-                        case SIXTEEN:
-                            updatedName = IMAGE_OUTPUT_DIR + SPLIT_TYPE.getFileNamePrefix() + "-" + fileName + "-" + hashMapBW16.get(total) + PNG;
+                        case SIXTEEN_ORIGINAL:
+                            updatedName = IMAGE_OUTPUT_DIR + SPLIT_TYPE.getFileNamePrefix() + "-" + fileName + "-" + bitWise48Full.get(total) + PNG;
+                            break;
+                        case SIXTEEN_MTN_WALL:
+                            updatedName = IMAGE_OUTPUT_DIR + SPLIT_TYPE.getFileNamePrefix() + "-" + fileName + "-" + bitWise48MtnWall.get(total) + PNG;
                             break;
                     }
 
